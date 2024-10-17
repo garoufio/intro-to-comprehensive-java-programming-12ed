fc32a3ac44386478c5131c819607cd945b6d8bea
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-10-17 12:51:41 +0300

    chapter7 exercises 7.36->7.37 initialization

---
+++ src/main/java/chapter7/Exercise7_36.java
@@ -0,0 +1,16 @@
+package chapter7;
+
+/**
+ * (Game: Eight Queens) The classic Eight Queens puzzle is to place eight queens on a chessboard such that no two queens
+ * can attack each other (i.e., no two queens are on the same row, same column, or same diagonal). The program displays
+ * one such solution.
+ */
+public class Exercise7_36 {
+  
+  public static void main(String[] args) {
+  
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  
+}
---
+++ src/main/java/chapter7/Exercise7_37.java
@@ -0,0 +1,20 @@
+package chapter7;
+
+/**
+ * (Game: bean machine) The bean machine, also known as a quincunx or the Galton box, is a device for statistics
+ * experiments named after English scientist Sir Francis Galton. It consists of an upright board with evenly spaced
+ * nails (or pegs) in a triangular form. Balls are dropped from the opening of the board. Every time a ball hits a nail,
+ * it has a 50% chance of falling to the left or to the right. The piles of balls are accumulated in the slots at the
+ * bottom of the board. The program should prompt the user to enter the number of the balls and the number of the slots
+ * in the machine. Simulate the falling of each ball by printing its path. Display the final buildup of the balls in
+ * the slots in a histogram.
+ */
+public class Exercise7_37 {
+  
+  public static void main(String[] args) {
+  
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  
+}
