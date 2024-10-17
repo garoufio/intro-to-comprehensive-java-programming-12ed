1fce8141e8f66b0bcee1da9e24b5322d91efc9e4
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-10-17 14:14:50 +0300

    update exercise 7.32

--- src/main/java/chapter7/Exercise7_32.java
+++ src/main/java/chapter7/Exercise7_32.java
@@ -1,5 +1,6 @@
 package chapter7;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercise7_32 {
@@ -10,7 +11,8 @@ public class Exercise7_32 {
     int[] list = readListContent(input, size);
     input.close();
     
-    // TODO: partition
+    partition(list);
+    System.out.println("After the partition, the list is: " + Arrays.toString(list));
   }
   
   //-------------------------------------------------------------------------------------------------------------------
@@ -35,6 +37,43 @@ public class Exercise7_32 {
       list[i] = scanner.nextInt();
     }
     return list;
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  
+  public static int partition(int[] list) {
+    int first = 0;
+    int last = list.length - 1;
+    int pivot = list[first]; // Choose the first element as the pivot
+    int low = first + 1; // Index for forward search
+    int high = last; // Index for backward search
+    
+    while (high > low) {
+      // Search forward from left
+      while (low <= high && list[low] <= pivot) low++;
+      
+      // Search backward from right
+      while (low <= high && list[high] > pivot) high--;
+      
+      // Swap two elements in the list
+      if (high > low) {
+        int temp = list[high];
+        list[high] = list[low];
+        list[low] = temp;
+      }
+    }
+    
+    while (high > first && list[high] >= pivot) high--;
+    
+    // Swap pivot with list[high]
+    if (pivot > list[high]) {
+      list[first] = list[high];
+      list[high] = pivot;
+      return high;
+    }
+    else {
+      return first;
+    }
   }
   
   //-------------------------------------------------------------------------------------------------------------------
