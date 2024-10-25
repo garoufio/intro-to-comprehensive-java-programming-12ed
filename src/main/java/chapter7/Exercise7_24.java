bfb4307fe467b0f7c9f1b170806edd64e7e1faf6
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-10-25 17:46:47 +0300

    add junit resources

--- src/main/java/chapter7/Exercise7_24.java
+++ src/main/java/chapter7/Exercise7_24.java
@@ -1,14 +1,43 @@
 package chapter7;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  *
  */
 public class Exercise7_24 {
   
   public static void main(String[] args) {
-    // TODO
+    List<String> list1 = List.of("Peter", "Sonia", "Paul");
+    List<String> list2 = Arrays.asList("Fenia", "Kate", "Dennis");
+    
+    List<String> list3 = new ArrayList<>(list1);
+    list3.addLast("Hector");
+    System.out.println(list3);
+    List<String> list4 = list2.stream().collect(Collectors.toList());
+    list4.addFirst("Hana");
+    System.out.println(list4);
+    
+    Item drinkItem = new DrinkItem("Milk", 1.89);
+    Item foodItem = new FoodItem("Yogurt", 4.59);
+    if (drinkItem instanceof DrinkItem item) {
+      var name = item.name();
+      var price = item.price();
+      System.out.printf("Name: %s, Price: %.2f\n", name, price);
+    }
+    if (foodItem instanceof FoodItem(String name, double price)) {
+      System.out.printf("Name: %s, Price: %.2f\n", name, price);
+    }
+    
   }
   
   //-------------------------------------------------------------------------------------------------------------------
+  
+  interface Item { };
+  public record FoodItem(String name, double price) implements Item { };
+  public record DrinkItem(String name, double price) implements Item { };
   
 }
