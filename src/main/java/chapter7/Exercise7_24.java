bf657b8d777a8f6ebc45cd3b3112c6cd0e6c65a5
Panagiotis Triantafyllos <ptriantap@gmail.com>
2024-10-31 17:30:57 +0200

    commit exercise 7.24

--- src/main/java/chapter7/Exercise7_24.java
+++ src/main/java/chapter7/Exercise7_24.java
@@ -1,43 +1,51 @@
 package chapter7;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
- *
+ * (Simulation: coupon collector’s problem) Coupon collector is a classic statistics problem with many practical
+ * applications. The problem is to repeatedly pick objects from a set of objects and find out how many picks are needed
+ * for all the objects to be picked at least once. A variation of the problem is to pick cards from a shuffled deck of
+ * 52 cards repeatedly, and find out how many picks are needed before you see one of each suit. Assume a picked card is
+ * placed back in the deck before picking another. The  program simulates the number of picks needed to get four cards
+ * from each suit and display the four cards picked (it is possible a card may be picked twice).
  */
 public class Exercise7_24 {
   
+  public static final int NUMBER_OF_CARDS = 52;
+  public static final String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
+  public static final String[] RANKS = {
+      "Ace", "2", "3", "4", "5",
+      "6", "7", "8", "9", "10",
+      "Jack", "Queen", "King"
+  };
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  
   public static void main(String[] args) {
-    List<String> list1 = List.of("Peter", "Sonia", "Paul");
-    List<String> list2 = Arrays.asList("Fenia", "Kate", "Dennis");
+    boolean[] pickedSuits = new boolean[SUITS.length];
+    int numberOfPicks = 0;
+    int cardNumber;
     
-    List<String> list3 = new ArrayList<>(list1);
-    list3.addLast("Hector");
-    System.out.println(list3);
-    List<String> list4 = list2.stream().collect(Collectors.toList());
-    list4.addFirst("Hana");
-    System.out.println(list4);
-    
-    Item drinkItem = new DrinkItem("Milk", 1.89);
-    Item foodItem = new FoodItem("Yogurt", 4.59);
-    if (drinkItem instanceof DrinkItem item) {
-      var name = item.name();
-      var price = item.price();
-      System.out.printf("Name: %s, Price: %.2f\n", name, price);
+    while (areAllSuitsPicked(pickedSuits) == false) {
+      cardNumber = (int) (Math.random() * NUMBER_OF_CARDS);
+      if (pickedSuits[cardNumber / 13] == false) {
+        pickedSuits[cardNumber / 13] = true;
+        
+        System.out.printf("%s of %s\n", RANKS[cardNumber % 13], SUITS[cardNumber / 13]);
+      }
+      numberOfPicks++;
     }
-    if (foodItem instanceof FoodItem(String name, double price)) {
-      System.out.printf("Name: %s, Price: %.2f\n", name, price);
-    }
-    
+    System.out.printf("Number of picks: %d\n", numberOfPicks);
   }
   
   //-------------------------------------------------------------------------------------------------------------------
   
-  interface Item { };
-  public record FoodItem(String name, double price) implements Item { };
-  public record DrinkItem(String name, double price) implements Item { };
+  public static boolean areAllSuitsPicked(boolean[] pickedSuits) {
+    for (int i = 0; i < pickedSuits.length; i++) {
+      if (pickedSuits[i] == false) return false;
+    }
+    return true;
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
   
 }
