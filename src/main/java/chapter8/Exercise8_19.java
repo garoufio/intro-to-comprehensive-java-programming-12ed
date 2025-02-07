79cc775ec3dd2ee2355ce0e573cc777b3947c91f
Panagiotis Triantafyllos <ptriantap@gmail.com>
2025-02-07 15:01:57 +0200

    update Exercise8_19

--- src/main/java/chapter8/Exercise8_19.java
+++ src/main/java/chapter8/Exercise8_19.java
@@ -12,69 +12,10 @@ public class Exercise8_19 {
   
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
-//      int rows = readSize(sc, "rows");
-//      int columns = readSize(sc, "columns");
-//      int[][] values = readArrayValues(sc, rows, columns);
-//      System.out.printf("Is consecutive four? %b", isConsecutiveFour(values));
-      
-//      int[][] test1 = new int[][] {
-//          {4, 5, 6, 7, 8, 9},
-//          {2, 1, 1, 1, 1, 3}
-//      };
-//      System.out.println("Check rows 1: " + checkRows(test1));
-//      System.out.println("Check columns 1: " + checkColumns(test1));
-//      int[][] test2 = new int[][] {
-//          {4, 5, 6, 7, 8, 9},
-//          {2, 1, 2, 1, 1, 3}
-//      };
-//      System.out.println("Check rows 2: " + checkRows(test2));
-//      System.out.println("Check columns 2: " + checkColumns(test2));
-//      int[][] test3 = new int[][] {
-//          {4, 5, 6, 7, 8, 9},
-//          {2, 1, 2, 1, 1, 3},
-//          {4, 5, 1, 6, 8, 9},
-//          {3, 5, 9, 7, 4, 3},
-//          {2, 5, 6, 2, 8, 9}
-//      };
-//      System.out.println("Check rows 3: " + checkRows(test3));
-//      System.out.println("Check columns 3: " + checkColumns(test3));
-//      int[][] test4 = new int[][] {
-//          {4, 7, 6, 7, 8, 9},
-//          {2, 5, 2, 1, 1, 3},
-//          {4, 5, 1, 6, 8, 9},
-//          {3, 5, 9, 7, 4, 3},
-//          {2, 5, 6, 2, 8, 9}
-//      };
-//      System.out.println("Check rows 4: " + checkRows(test4));
-//      System.out.printf("Check columns 4: %b\n", checkColumns(test4));
-//      System.out.println("Check columns 3: " + checkColumns(test3));
-      int[][] test5 = new int[][] {
-          {4, 7, 6, 7, 8, 9},
-          {2, 5, 2, 1, 1, 3},
-          {4, 5, 5, 6, 8, 9},
-          {3, 5, 9, 5, 4, 3},
-          {2, 5, 6, 2, 5, 9}
-      };
-      System.out.println("Check major diagonal 5: " + checkMajorDiagonal(test5));
-      System.out.printf("Check sub diagonal 5: %b\n", checkSubDiagonal(test5));
-      int[][] test6 = new int[][] {
-          {4, 7, 6, 7, 8, 9},
-          {2, 5, 2, 1, 9, 3},
-          {4, 5, 1, 9, 8, 9},
-          {3, 5, 9, 7, 4, 3},
-          {2, 5, 6, 2, 8, 9}
-      };
-      System.out.println("Check major diagonal 6: " + checkMajorDiagonal(test6));
-      System.out.printf("Check sub diagonal 6: %b\n", checkSubDiagonal(test6));
-      int[][] test7 = new int[][] {
-          {4, 7, 6, 7, 8, 9},
-          {2, 5, 2, 1, 7, 3},
-          {4, 5, 5, 9, 8, 9},
-          {3, 5, 9, 5, 4, 3},
-          {2, 5, 6, 2, 8, 9}
-      };
-      System.out.println("Check major diagonal 7: " + checkMajorDiagonal(test7));
-      System.out.printf("Check sub diagonal 7: %b\n", checkSubDiagonal(test7));
+      int rows = readSize(sc, "rows");
+      int columns = readSize(sc, "columns");
+      int[][] values = readArrayValues(sc, rows, columns);
+      System.out.printf("Is consecutive four? %b", isConsecutiveFour(values));
     }
     catch (InputMismatchException e) {
       System.out.println("Invalid input! " + e.getMessage());
@@ -123,16 +64,17 @@ public class Exercise8_19 {
   //-------------------------------------------------------------------------------------------------------------------
   
   public static boolean checkRows(int[][] values) {
-    int count;
-    for (int row = 0; row < values.length; row++) { // rows
-      count = 0;
-      for (int column = 0; column < values[row].length - 1; column++) { // columns
-        if (values[row][column] == values[row][column + 1]) {
-          count++;
-          if (count == 3) return true;
-          else continue;
+    boolean hasConsecutiveFour;
+    for (int row = 0; row < values.length; row++) { // check every row
+      for (int column = 0; column <= values[row].length - 4; column++) {
+        hasConsecutiveFour = true;
+        for (int i = column; i < column + 3; i++) { // check columns by a window of 4 values
+          if (values[row][i] != values[row][i + 1]) {
+            hasConsecutiveFour = false;
+            break;
+          }
         }
-        count = 0;
+        if (hasConsecutiveFour) return true;
       }
     }
     return false;
@@ -141,16 +83,17 @@ public class Exercise8_19 {
   //-------------------------------------------------------------------------------------------------------------------
   
   public static boolean checkColumns(int[][] values) {
-    int count;
-    for (int column = 0; column < values[0].length; column++) { // columns
-      count = 0;
-      for (int row = 0; row < values.length - 1; row++) { // rows
-        if (values[row][column] == values[row + 1][column]) {
-          count++;
-          if (count == 3) return true;
-          else continue;
+    boolean hasConsecutiveFour;
+    for (int column = 0; column < values[0].length; column++) { // check every column
+      for (int row = 0; row <= values.length - 4; row++) {
+        hasConsecutiveFour = true;
+        for (int i = row; i < row + 3; i++) { // check rows by a window of 4 values
+          if (values[i][column] != values[i + 1][column]) {
+            hasConsecutiveFour = false;
+            break;
+          }
         }
-        count = 0;
+        if (hasConsecutiveFour) return true;
       }
     }
     return false;
@@ -158,31 +101,43 @@ public class Exercise8_19 {
   
   //-------------------------------------------------------------------------------------------------------------------
   
-  public static boolean checkMajorDiagonal(int[][] values) {
-    // check right part
-    for (int i = 0; i < values.length; i++) {
-      for (int j = 0; j < values[i].length; j++) {
-      
+  public static boolean checkDiagonals(int[][] values) {
+    boolean hasConsecutiveFour;
+    
+    // check from left to right
+    for (int row = 0; row <= values.length - 4; row++) {
+      for (int column = 0; column <= values[row].length - 4; column++) {
+        hasConsecutiveFour = true;
+        for (int i = row, j = column; i < row + 3 && j < column + 3; i++, j++) {
+          if (values[i][j] != values[i + 1][j + 1]) {
+            hasConsecutiveFour = false;
+            break;
+          }
+        }
+        if (hasConsecutiveFour) return true;
       }
     }
     
-    // check left part
-    return false;
-  }
-  
-  //-------------------------------------------------------------------------------------------------------------------
-  
-  public static boolean checkSubDiagonal(int[][] values) {
-    // check right part
-    
-    // check left part
+    // check from right to left
+    for (int row = 0; row <= values.length - 4; row++) {
+      for (int column = values[row].length - 1; column >= 3; column--) {
+        hasConsecutiveFour = true;
+        for (int i = row, j = column; i < row + 3 && j > column - 3; i++, j--) {
+          if (values[i][j] != values[i + 1][j - 1]) {
+            hasConsecutiveFour = false;
+            break;
+          }
+        }
+        if (hasConsecutiveFour) return true;
+      }
+    }
     return false;
   }
   
   //-------------------------------------------------------------------------------------------------------------------
   
   public static boolean isConsecutiveFour(int[][] values) {
-    return checkColumns(values) || checkColumns(values) || checkMajorDiagonal(values) || checkSubDiagonal(values);
+    return checkRows(values) || checkColumns(values) || checkDiagonals(values);
   }
   
   //-------------------------------------------------------------------------------------------------------------------
